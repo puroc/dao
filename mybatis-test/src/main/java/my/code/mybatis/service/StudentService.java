f74f4d60672cdd7bae4342e14b223f0fcc8669e6
pud <471049931@qq.com>
2014-05-18 18:02:11 +0800

    Signed-off-by: pud <471049931@qq.com>

--- mybatis-test/src/main/java/my/code/mybatis/service/StudentService.java
+++ mybatis-test/src/main/java/my/code/mybatis/service/StudentService.java
@@ -1,25 +1,32 @@
 package my.code.mybatis.service;
 
+import java.util.List;
+
 import my.code.mybatis.domain.Student;
 import my.code.mybatis.mapper.StudentMapper;
 import my.code.mybatis.exception.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
-	
+
 	@Autowired
 	private StudentMapper studentMapper;
-	
-	@Transactional(rollbackFor=DbException.class)
-	public void insert(String name) throws DbException{
+
+	@Transactional(rollbackFor = DbException.class)
+	public void insert(String name) throws DbException {
 		Student student = new Student();
 		student.setAge(34);
 		student.setName(name);
 		studentMapper.insert(student);
-		throw new DbException("insert failed.");
+//		throw new DbException("insert failed.");
+	}
+
+	public List<Student> select(String name) throws DbException {
+		return studentMapper.select(name);
 	}
 
 }
--- mybatis-test/src/test/java/my/code/mybatis/StudentServiceTest.java
+++ mybatis-test/src/test/java/my/code/mybatis/StudentServiceTest.java
@@ -3,6 +3,9 @@
  */
 package my.code.mybatis;
 
+import java.util.List;
+
+import my.code.mybatis.domain.Student;
 import my.code.mybatis.exception.DbException;
 import my.code.mybatis.service.StudentService;
 
@@ -14,18 +17,28 @@ import org.unitils.spring.annotation.SpringBeanByName;
 
 /**
  * @author pud
- *
+ * 
  */
 @SpringApplicationContext("spring-beans.xml")
-public class StudentServiceTest extends UnitilsJUnit4{
+public class StudentServiceTest extends UnitilsJUnit4 {
 
 	@SpringBeanByName
 	private StudentService studentService;
-	
+
 	@Test
-	public void testInsert(){
+	public void testInsert() {
 		try {
 			studentService.insert("haha");
+		} catch (DbException e) {
+			Assert.fail(e.getMessage());
+		}
+	}
+
+	@Test
+	public void testQuery() {
+		try {
+			List<Student> list = studentService.select("haha");
+			Assert.assertTrue(!list.isEmpty());
 		} catch (DbException e) {
 			Assert.fail(e.getMessage());
 		}
